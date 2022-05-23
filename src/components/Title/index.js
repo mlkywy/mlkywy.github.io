0a26664197dac25bcbcad6c67045a8405ee955a4
alsheikhiu <alsheikh@iu.edu>
2022-05-23 02:44:52 -0400

    Typewriter effect

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { GithubLogo, LinkedinLogo, TwitterLogo } from "phosphor-react";
+import Typewriter from "typewriter-effect";
 
 const Title = () => {
   return (
@@ -7,8 +8,16 @@ const Title = () => {
       <div className="w-80 text-5xl p-3  text-dark1 dark:text-light1 flex flex-col items-center text-center font-monospace">
         Hi, I'm Alina!
         <div className="w-full text-lg font-bold font-sans">
-          I'm a recent graduate and aspiring software developer, based in the
-          US.
+          <Typewriter
+            options={{
+              delay: 85,
+            }}
+            onInit={(typewriter) => {
+              typewriter
+                .start()
+                .typeString("Recent graduate and web developer. ");
+            }}
+          />
         </div>
         <div className="w-28 mt-8 border-t-4 border-accent text-middle text-3xl font-normal flex flex-row justify-between py-6">
           <a href="https://github.com/alshei" target="_blank">
