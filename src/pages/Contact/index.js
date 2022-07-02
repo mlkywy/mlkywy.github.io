111e56e52c0c867fb27d9337b0d91aec08ba4a3b
alsheikhiu <alsheikh@iu.edu>
2022-07-02 12:27:54 -0400

    Update index.js

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -4,7 +4,7 @@ import Form from "../../components/Form";
 const Contact = () => {
   return (
     <div id="contact" className="bg-light1 dark:bg-dark1 w-screen pt-20">
-      <div className="container px-8 md:px-4 lg:px-44 mx-auto flex h-full pt-16 pb-40 items-center justify-center">
+      <div className="container px-8 md:px-4 lg:px-44 mx-auto flex h-full pt-16 pb-32 items-center justify-center">
         <Form />
       </div>
     </div>
