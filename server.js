04f479db0302b168df98b3cb941adab2f2e26019
alsheikhiu <alsheikh@iu.edu>
2022-07-02 00:19:46 -0400

    working contact form!

--- server.js
+++ server.js
@@ -1,12 +1,52 @@
 const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+
+require("dotenv").config();
+
+const { PORT, EMAIL, PASSWORD } = process.env;
+
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}!`));
 
-const PORT = process.env.PORT || 5000;
-
-app.get("/", (req, res) => {
-  res.send("hello!!");
+const contactEmail = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: EMAIL,
+    pass: PASSWORD,
+  },
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}!`);
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to send!");
+  }
+});
+
+router.post("/contact", (req, res) => {
+  const firstname = req.body.firstname;
+  const lastname = req.body.lastname;
+  const email = req.body.email;
+  const message = req.body.message;
+  const mail = {
+    from: firstname,
+    to: EMAIL,
+    subject: "Contact Form Submission",
+    html: `<p>Name: ${firstname} ${lastname}</p>
+           <p>Email: ${email}</p>
+           <p>Message: ${message}</p>`,
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json({ status: "ERROR" });
+    } else {
+      res.json({ status: "Message Sent" });
+    }
+  });
 });
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { ContactButton } from "../Buttons";
 
 const Form = () => {
+  const [status, setStatus] = useState("Submit");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setStatus("Sending...");
+
+    const { firstname, lastname, email, message } = e.target.elements;
+
+    let details = {
+      firstname: firstname.value,
+      lastname: lastname.value,
+      email: email.value,
+      message: message.value,
+    };
+
+    let response = await fetch("http://localhost:5000/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(details),
+    });
+
+    setStatus("Submit");
+    let result = await response.json();
+    alert(result.status);
+
+    // clear all input values in the form
+    e.target.reset();
+  };
   return (
     <>
-      <form className="w-full max-w-lg">
+      <form className="w-full max-w-lg" onSubmit={handleSubmit}>
         <div className="flex flex-wrap -mx-3 mb-6">
           <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
             <label
@@ -15,9 +45,10 @@ const Form = () => {
             </label>
             <input
               className="appearance-none block w-full bg-light2 text-dark2 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              id="grid-first-name"
+              id="firstname"
               type="text"
               placeholder="First"
+              required
             />
           </div>
           <div className="w-full md:w-1/2 px-3">
@@ -29,9 +60,10 @@ const Form = () => {
             </label>
             <input
               className="appearance-none block w-full bg-light2 text-dark2 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white"
-              id="grid-last-name"
+              id="lastname"
               type="text"
               placeholder="Last"
+              required
             />
           </div>
         </div>
@@ -48,6 +80,7 @@ const Form = () => {
               id="email"
               type="email"
               placeholder="email@address.com"
+              required
             />
           </div>
         </div>
@@ -63,12 +96,15 @@ const Form = () => {
               className=" no-resize appearance-none block w-full bg-light2 text-dark2 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white h-48 resize-none"
               id="message"
               placeholder="Let me know what you think!"
+              required
             ></textarea>
           </div>
         </div>
         <div className="md:flex md:items-center">
           <div className="md:w-1/3">
-            <ContactButton link="" option="Submit" />
+            <ContactButton link="" option="Submit" type="submit">
+              {status}
+            </ContactButton>
           </div>
           <div className="md:w-2/3"></div>
         </div>
