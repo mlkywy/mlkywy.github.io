40b919576d773df105fe44c588ee4e94516917de
alsheikhiu <alsheikh@iu.edu>
2022-07-02 13:20:45 -0400

    emailjs set up

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,10 +1,45 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { ContactButton } from "../Buttons";
+import emailjs from "@emailjs/browser";
 
 const Form = () => {
+  const [showSuccess, setShowSuccess] = useState(false);
+  const [showError, setShowError] = useState(false);
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    setShowSuccess(false);
+    setShowError(false);
+
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICEID,
+        process.env.REACT_APP_TEMPLATEID,
+        form.current,
+        process.env.REACT_APP_USERID
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setShowSuccess(true);
+          setShowError(false);
+        },
+        (error) => {
+          console.log(error.text);
+          setShowSuccess(false);
+          setShowError(true);
+        }
+      );
+
+    // clear all input values in the form
+    e.target.reset();
+  };
   return (
     <>
-      <form className="w-full max-w-lg" onSubmit="">
+      <form ref={form} className="w-full max-w-lg" onSubmit={sendEmail}>
         <div className="flex flex-wrap -mx-3 mb-6">
           <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
             <label
@@ -16,6 +51,7 @@ const Form = () => {
             <input
               className="appearance-none block w-full bg-light2 text-dark2 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white text-sm lg:text-base md:text-base"
               id="firstname"
+              name="firstname"
               type="text"
               placeholder="First"
               required
@@ -31,6 +67,7 @@ const Form = () => {
             <input
               className="appearance-none block w-full bg-light2 text-dark2 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white text-sm lg:text-base md:text-base"
               id="lastname"
+              name="lastname"
               type="text"
               placeholder="Last"
               required
@@ -48,6 +85,7 @@ const Form = () => {
             <input
               className="appearance-none block w-full bg-light2 text-dark2 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white text-sm lg:text-base md:text-base"
               id="email"
+              name="email"
               type="email"
               placeholder="email@address.com"
               required
@@ -58,27 +96,33 @@ const Form = () => {
           <div className="w-full px-3">
             <label
               className="block uppercase tracking-wide text-accent text-xs font-bold mb-2"
-              for="grid-password"
+              for="grid-message"
             >
               Message
             </label>
             <textarea
               className="no-resize appearance-none block w-full bg-light2 text-dark2 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white h-48 resize-none text-sm lg:text-base md:text-base"
               id="message"
+              name="message"
               placeholder="Let me know what you think!"
               required
             ></textarea>
           </div>
         </div>
-        <div className="md:flex md:items-center">
-          <div className="md:w-1/3">
-            <ContactButton
-              link=""
-              option="submit"
-              type="submit"
-            ></ContactButton>
+        <div className="flex flex-row justify-between items-center">
+          <div>
+            <ContactButton option="submit" type="submit" />
           </div>
-          <div className="md:w-2/3"></div>
+          {showSuccess && (
+            <div className="text-sm md:text-base lg:text-base text-accent">
+              <i>Your message was sent successfully!</i>
+            </div>
+          )}
+          {showError && (
+            <div className="text-sm md:text-base lg:text-base text-accent">
+              <i>Uh, oh! Something went wrong.</i>
+            </div>
+          )}
         </div>
       </form>
     </>
